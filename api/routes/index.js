921582cb3bedac5fb74355f10f2ddf3602ce67e9
Jonas Marinho <jonasmarinho@brain-ubuntu-vm.quvpja1a1jjelda1b4wvbpt3jg.jx.internal.cloudapp.net>
2021-06-12 17:42:13 +0000

    Sistema de rotas

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	return res.send({message: "API do brAIn funcionando"});
+	return res.send({message: "API geral do brAIn funcionando"});
 });
 
 module.exports = router;
