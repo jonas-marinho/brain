515bdb8cd659d9d2beb6b2cdf2db28f556d626ea
Jonas Marinho <jonasmarinho95@gmail.com>
2021-06-12 14:55:21 -0300

    Testando o POST

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-		return res.send({message: "API de usuÃ¡rios do brAIn funcionando"});
+		return res.send({message: "API de usuários do brAIn funcionando"});
+});
+
+router.post('/auth', (req, res) => {
+		return res.send("Retornar se o usuário recebeu permissao para logar ou nao");
 });
 
 module.exports = router;
