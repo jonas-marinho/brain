6f0f854070eda01335cc959dc3a60f2f9f54f3f9
Jonas Marinho <jonasmarinho95@gmail.com>
2021-06-14 22:07:20 -0300

    Adicionando criptografia para password e userToken

--- api/model/user.js
+++ api/model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
 	email: {type: String, required: true, unique: true, lowercase: true},
@@ -8,6 +9,21 @@ const UserSchema = new Schema({
 	userToken: {type: String, required: true, select: false},
 	name: {type: String, default: 'User'},
 	created: {type: Date, default: Date.now}
+});
+
+UserSchema.pre('save', function(next){
+	let user = this;
+	if(!user.isModified('password') || !user.isModified('userToken')) return next();
+	
+	bcrypt.hash(user.password, 10, (err, encrypted) => {
+		user.password = encrypted;
+	});
+	
+	bcrypt.hash(user.userToken, 10, (err, encrypted) => {
+		user.userToken = encrypted;
+	});
+	
+	return next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
