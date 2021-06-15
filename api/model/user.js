db03e29a02216eaa2af5b5e97f24cbef6e6ee981
Jonas Marinho <jonasmarinho95@gmail.com>
2021-06-14 22:24:51 -0300

    Adicionando auto increment para userID

--- api/model/user.js
+++ api/model/user.js
@@ -5,12 +5,13 @@ const bcrypt = require('bcrypt');
 const UserSchema = new Schema({
 	email: {type: String, required: true, unique: true, lowercase: true},
 	password: {type: String, required: true, select: false},
-	userID: {type: String, required: true, unique: true},
+	userID: {type: String, unique: true},
 	userToken: {type: String, required: true, select: false},
 	name: {type: String, default: 'User'},
 	created: {type: Date, default: Date.now}
 });
 
+// Encrypt password
 UserSchema.pre('save', function(next){
 	let user = this;
 	if(!user.isModified('password')) return next();
@@ -21,14 +22,28 @@ UserSchema.pre('save', function(next){
 	});
 });
 
+// Generate the internal userToken using the email as seed
 UserSchema.pre('save', function(next){
 	let user = this;
-	if(!user.isModified('userToken')) return next();
+	if(!user.isModified('email')) return next();
 	
-	bcrypt.hash(user.userToken, 10, (err, encrypted) => {
+	bcrypt.hash(user.email, 10, (err, encrypted) => {
 		user.userToken = encrypted;
 		return next();
 	});
+});
+
+// Calculate the userID auto increment
+UserSchema.pre('save', function (next) {
+	let user = this;
+	if(!(user.userID!=0)) return next();
+	mongoose
+		.model("userID", UserSchema)
+		.countDocuments({ account: User.account }, function (err, counter) {
+			if (err) return next(err);
+			user.userID = counter + 1;
+			next();
+		});
 });
 
 module.exports = mongoose.model('User', UserSchema);
