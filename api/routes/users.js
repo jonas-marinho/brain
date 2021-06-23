26392ef5e2d93302466b51b77e5297a86f90874f
Jonas Marinho <jonasmarinho95@gmail.com>
2021-06-23 19:31:44 -0300

    Adicionando os status code

--- api/routes/users.js
+++ api/routes/users.js
@@ -10,32 +10,32 @@ router.get('/', async (req, res) => {
 		return res.send(users);
 	}
 	catch (err) {
-		return res.send({error:"Error on users consult"});
+		return res.status(500).send({error:"Error on users consult"});
 	}
 });
 
 // Autenticação
 router.get('/auth', (req, res) => {
-		return res.send({message:"To get data from this API endpoint, use POST method, passing email and password as parameters."});
+		return res.status(400).send({message:"To get data from this API endpoint, use POST method, passing email and password as parameters."});
 });
 router.post('/auth', async (req, res) => {
 	const {email, password} = req.body;
-	if (!email || !password) return res.send({ error: 'Send an email and a password' });
+	if (!email || !password) return res.status(400).send({ error: 'Send an email and a password' });
 	
 	try {
 		const user = await User.findOne({email}).select('+password');
-		if (!user) return res.send({ error: 'This email is not registered' });
+		if (!user) return res.status(401).send({ error: 'This email is not registered' });
 		
 		const rightPassword = await bcrypt.compare(password, user.password);
 		
-		if(!rightPassword) return res.send({ error: 'The password is wrong' });
+		if(!rightPassword) return res.status(401).send({ error: 'The password is wrong' });
 		
 		user.password = undefined;
-		return res.send(user);
+		return res.status(202).send(user);
 		
     }
 	catch (err) {
-		return res.send({ error: 'Error finding user' });
+		return res.status(500).send({ error: 'Error finding user' });
 	}
 });
 
@@ -43,17 +43,17 @@ router.post('/auth', async (req, res) => {
 router.post('/create', async (req, res) => {
 	const {email, password, name} = req.body;
 	
-	if (!email || !password || !name) return res.send({error: "There are fields that are not filled"});
+	if (!email || !password || !name) return res.status(400).send({error: "There are fields that are not filled"});
 	
 	try {
-		if(await User.findOne({email})) return res.send({error: "The user already exists"});
+		if(await User.findOne({email})) return res.status(400).send({error: "The user already exists"});
 		
 		createdUser = await User.create({email: email, password: password, name:name});
 		createdUser.password = undefined;
-		return res.send(createdUser);
+		return res.status(201).send(createdUser);
 	}
 	catch (err) {
-		return res.send({error: "Error searching for user"});
+		return res.status(500).send({error: "Error searching for user"});
 	}
 });
 
