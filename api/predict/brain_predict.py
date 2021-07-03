63820a657840a074abb3fcbc6f0d4d73441fd164
Jonas Marinho <jonasmarinho95@gmail.com>
2021-07-03 20:33:59 -0300

    Atualizando o script de predicao

--- api/predict/brain_predict.py
+++ api/predict/brain_predict.py
@@ -14,6 +14,7 @@ if len(sys.argv) == 2:
     raw_data = str(sys.argv[1])
 else:
     print('{"error":"There are no data received"}')
+    exit()
     
 
 # Transformar a string em uma matriz
--- api/predict/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-print(str(len(sys.argv) != 2))
-
-# Ler os dados que são passados como parâmetro
-if len(sys.argv) == 2:
-    raw_data = str(sys.argv[1])
-else:
-    print('{"error":"There are no data received"}')
