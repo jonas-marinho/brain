34504f879031b2844a73e633572c2b9550a56a9b
Jonas Marinho <jonasmarinho95@gmail.com>
2021-09-05 13:57:28 -0300

    Criando o sistema de autenticação

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -2,11 +2,12 @@
 
 // Função autenticação
 const auth = (req, res, next) => {
+return next(); // Esta linha desativa a autenticação temporariamente
 	const tokenHeader = req.headers.auth;
 	if(!tokenHeader) return res.status(403).send({"error":"To access this endpoint, send the user's token"});
 	jwt.verify(tokenHeader, 'brain', (err, decoded) =>
 	{
-		if(err) return res.status(403).send({"error":"This user token is not valid for this user"});
+		if(err) return res.status(403).send({"error":"This user token is not valid"});
 		return next();
 	});
 };
