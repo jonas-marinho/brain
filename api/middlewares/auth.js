fbb2ffdc805f4c998b3d77716263d84b16120ed5
Jonas Marinho <jonasmarinho95@gmail.com>
2021-09-05 13:22:49 -0300

    Criando o sistema de autenticação

---
+++ api/middlewares/auth.js
@@ -0,0 +1,9 @@
+﻿const express = require('express');
+const router = express.Router();
+
+// Mensagem padrão desta rota
+router.get('/', (req, res) => {
+		return res.send({message: "brAIn's general API is working correctly!"});
+});
+
+module.exports = router;
