86db9494d593480f5c0ab3341e3105baace20a06
Jonas Marinho <jonasmarinho95@gmail.com>
2021-09-05 14:19:23 -0300

    Criando o sistema de autenticação

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -3,7 +3,7 @@ const config = require('../config/config');
 
 // Função autenticação
 const auth = (req, res, next) => {
-//return next(); // Esta linha desativa a autenticação temporariamente, para fins de desenvolvimento e testes
+return next(); // Esta linha desativa a autenticação temporariamente, para fins de desenvolvimento e testes
 	const tokenHeader = req.headers.token;
 	if(!tokenHeader) return res.status(403).send({"error":"To access this endpoint, send the user's token"});
 	jwt.verify(tokenHeader, config.jwt_password, (err, decoded) =>
